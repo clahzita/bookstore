d7726f8c3d423f0f6861190601894f2d514609b0
Clarissa Soares <clah@ufrn.edu.br>
2019-10-27 20:08:54 -0300

    Initializing the project

---
+++ src/main/java/com/bookstore/codehome/CodehomeApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore.codehome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodehomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodehomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookstore/codehome/CodehomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstore.codehome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodehomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
