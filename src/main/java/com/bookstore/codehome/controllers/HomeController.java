74572cdbfd421573943d75cbc9f5419760d7bebd
Clarissa Soares <clah@ufrn.edu.br>
2019-10-27 23:12:26 -0300

    adding some dependencies and home controller

---
+++ src/main/java/com/bookstore/codehome/controllers/HomeController.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.bookstore.codehome.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Home page controller
+ * 
+ * @author clarissa
+ *
+ */
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public void index() {
+		System.out.println("Carregando produtos");
+	}
+
+}
--- src/test/java/com/bookstore/codehome/CodehomeApplicationTests.java
+++ src/test/java/com/bookstore/codehome/CodehomeApplicationTests.java
@@ -1,6 +1,6 @@
 package com.bookstore.codehome;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
