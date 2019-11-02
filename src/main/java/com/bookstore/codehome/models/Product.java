14f40a89a6219882b3ca807dcace562b958f903c
Clarissa Soares <clah@ufrn.edu.br>
2019-11-02 00:10:38 -0300

    lombok removed, app can insert, delete and edit a product

--- src/main/java/com/bookstore/codehome/models/Product.java
+++ src/main/java/com/bookstore/codehome/models/Product.java
@@ -9,52 +9,114 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-import org.springframework.data.annotation.Id;
 import org.springframework.format.annotation.DateTimeFormat;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 /**
  * @author clarissa
  *
  */
 @Entity
-@NoArgsConstructor @AllArgsConstructor
-@ToString
 public class Product implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 
-	@Id @GeneratedValue
-	@Setter private Long id; 
+	@Id
+	@SequenceGenerator(name = "product_seq", sequenceName = "product_seq")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_seq")
+	private Long id; 
 	
 	@Column(nullable = false, length = 150)
 	@NotBlank(message = "Title is a required information.")
-	@Getter @Setter private String title;
+	private String title;
 	
 	@Column(nullable = false, length = 150)
 	@NotBlank(message = "Author is a required information.")
-	@Getter @Setter private String author;
+	private String author;
 	
 	@Column(nullable = false, length = 255)
 	@NotBlank(message = "Description is a required information.")
-	@Getter @Setter private String description;
+	private String description;
 	
-	@Getter @Setter private Integer pages;
+	private Integer pages;
 	
 	@Column(nullable = false)
 	@Temporal(TemporalType.DATE)
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	@NotNull(message = "Publication Date is a required information.")
-	@Getter @Setter private Date publicationDate;
+	private Date publicationDate;
+
+	public Product() {}
+			
+	public Product(Long id, @NotBlank(message = "Title is a required information.") String title,
+			@NotBlank(message = "Author is a required information.") String author,
+			@NotBlank(message = "Description is a required information.") String description, Integer pages,
+			@NotNull(message = "Publication Date is a required information.") Date publicationDate) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.description = description;
+		this.pages = pages;
+		this.publicationDate = publicationDate;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getPages() {
+		return pages;
+	}
+
+	public void setPages(Integer pages) {
+		this.pages = pages;
+	}
+
+	public Date getPublicationDate() {
+		return publicationDate;
+	}
+
+	public void setPublicationDate(Date publicationDate) {
+		this.publicationDate = publicationDate;
+	}
+	
+	
+	
 	
 }
---
+++ src/test/java/com/bookstore/codehome/models/ProductTest.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.bookstore.codehome.models;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author clarissa
+ *
+ */
+public class ProductTest {
+
+	@Test
+	public void test() {
+		Product p =  new Product();
+		
+		fail("Not yet implemented");
+	}
+
+}
